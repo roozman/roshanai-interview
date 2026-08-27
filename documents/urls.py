25994d5083ac33763bde32661c68e42a647a5ae9
roozman <roozbeh.hmn@proton.me>
2026-08-27 12:17:03 +0330

    feat: add document CRUD API

---
+++ documents/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+
+from documents.views import DocumentViewSet
+
+
+router = DefaultRouter()
+router.register(
+    "documents",
+    DocumentViewSet,
+    basename="document",
+)
+
+urlpatterns = router.urls
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.parsers import (
+    FormParser,
+    JSONParser,
+    MultiPartParser,
+)
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from documents.models import Document
+from documents.serializers import (
+    DocumentDetailSerializer,
+    DocumentListSerializer,
+)
+from documents.services.ingestion import process_document
+
+
+class DocumentViewSet(ModelViewSet):
+    queryset = Document.objects.all()
+
+    parser_classes = (
+        JSONParser,
+        FormParser,
+        MultiPartParser,
+    )
+
+    http_method_names = (
+        "get",
+        "post",
+        "patch",
+        "delete",
+        "head",
+        "options",
+    )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return DocumentListSerializer
+
+        return DocumentDetailSerializer
+
+    def perform_create(self, serializer):
+        document = serializer.save()
+        serializer.instance = process_document(document.pk)
+
+    def perform_update(self, serializer):
+        file_changed = "file" in serializer.validated_data
+        document = serializer.save()
+
+        if file_changed:
+            serializer.instance = process_document(document.pk)
